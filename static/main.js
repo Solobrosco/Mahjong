7e4e821e5fce6f534741da1a116af66af45ed5be
Nikko <nikkosolon24@gmail.com>
2019-12-16 10:23:00 -0800

    Created class in main.js

--- static/main.js
+++ static/main.js
@@ -1,50 +1,20 @@
-function getHand(eHand, nHand, wHand, sHand){
-    var r = 0;
-    for (var i = 0; i < 14; i++){
-        if(i < 10){
-            var EID = "#E0" + i.toString();
-            var NID = "#N0" + i.toString();
-            var WID = "#W0" + i.toString();
-            var SID = "#S0" + i.toString();
-        }else{
-            var EID = "#E" + i.toString();
-            var NID = "#N" + i.toString();
-            var WID = "#W" + i.toString();
-            var SID = "#S" + i.toString();
-        }
-        $(EID).html(eHand[r][i]);
-        $(NID).html(nHand[r][i]);
-        $(WID).html(wHand[r][i]);
-        $(SID).html(sHand[r][i]);
+class Majong{
+    constructor(data){
+        this.garbage = data["garbage"];
+        this.eHand = data["eastHand"];
+        this.nHand = data["northHand"];
+        this.wHand = data["westHand"];
+        this.sHand = data["southHand"];
     }
-}
-
-$(document).ready(function(){
-	$.get("/init", {}, function(response){
-        var data = JSON.parse(response);
-        var garbage = data["garbage"];
-        var eHand = data["eastHand"];
-        var nHand = data["northHand"];
-        var wHand = data["westHand"];
-        var sHand = data["southHand"];
-        $("#trash").html(garbage);
-        getHand(eHand, nHand, wHand, sHand);
-        $("#trash").click({hand: i = 0, player: j = -1},function(e){
-            var x = e.data.player;
-            var y = e.data.hand;
-            console.log("(Player: " + x + ", Tile: " + y + ") Clicked");
-            $.get("/handle", {"x": x, "y": y, "btn": "left"}, function(response){
-                var data = JSON.parse(response);
-                var garbage = data["garbage"];
-                var eHand = data["eastHand"];
-                var nHand = data["northHand"];
-                var wHand = data["westHand"];
-                var sHand = data["southHand"];
-                $("#trash").html(garbage);
-                getHand(eHand, nHand, wHand, sHand);    
-            });
-        });
-
+    setBoard(data){
+        this.garbage = data["garbage"];
+        this.eHand = data["eastHand"];
+        this.nHand = data["northHand"];
+        this.wHand = data["westHand"];
+        this.sHand = data["southHand"];
+    }
+    getHand(){
+        var r = 0;
         for (var i = 0; i < 14; i++){
             if(i < 10){
                 var EID = "#E0" + i.toString();
@@ -57,20 +27,55 @@ $(document).ready(function(){
                 var WID = "#W" + i.toString();
                 var SID = "#S" + i.toString();
             }
-            
+            $(EID).html(this.eHand[r][i]);
+            $(NID).html(this.nHand[r][i]);
+            $(WID).html(this.wHand[r][i]);
+            $(SID).html(this.sHand[r][i]);
+        }
+    }
+    getGarbage(){
+        return this.garbage;
+    }
+}
+
+$(document).ready(function(){
+	$.get("/init", {}, function(response){
+        var data = JSON.parse(response);
+        let game = new Majong(data);
+        $("#trash").html(game.getGarbage());
+        game.getHand();
+        $("#trash").click({hand: i = 0, player: j = -1},function(e){
+            var x = e.data.player;
+            var y = e.data.hand;
+            console.log("(Player: " + x + ", Tile: " + y + ") Clicked");
+            $.get("/handle", {"x": x, "y": y, "btn": "left"}, function(response){
+                var data = JSON.parse(response);
+                game.setBoard(data);
+                $("#trash").html(game.getGarbage());
+                game.getHand();    
+            });
+        });
+        for (var i = 0; i < 14; i++){
+            if(i < 10){
+                var EID = "#E0" + i.toString();
+                var NID = "#N0" + i.toString();
+                var WID = "#W0" + i.toString();
+                var SID = "#S0" + i.toString();
+            }else{
+                var EID = "#E" + i.toString();
+                var NID = "#N" + i.toString();
+                var WID = "#W" + i.toString();
+                var SID = "#S" + i.toString();
+            }
             $(EID).click({hand: i, player: j = 0},function(e){
                 var x = e.data.player;
                 var y = e.data.hand;
                 console.log("(Player: " + x + ", Tile: " + y + ") Clicked");
                 $.get("/handle", {"x": x, "y": y, "btn": "left"}, function(response){
                     var data = JSON.parse(response);
-                    var garbage = data["garbage"];
-                    var eHand = data["eastHand"];
-                    var nHand = data["northHand"];
-                    var wHand = data["westHand"];
-                    var sHand = data["southHand"];
-                    $("#trash").html(garbage);
-                    getHand(eHand, nHand, wHand, sHand);    
+                    game.setBoard(data);
+                    $("#trash").html(game.getGarbage());
+                    game.getHand();   
                 });
             });
             $(SID).click({hand: i, player: j = 1},function(e){
@@ -79,13 +84,9 @@ $(document).ready(function(){
                 console.log("(Player: " + x + ", Tile: " + y + ") Clicked");
                 $.get("/handle", {"x": x, "y": y, "btn": "left"}, function(response){
                     var data = JSON.parse(response);
-                    var garbage = data["garbage"];
-                    var eHand = data["eastHand"];
-                    var nHand = data["northHand"];
-                    var wHand = data["westHand"];
-                    var sHand = data["southHand"];
-                    $("#trash").html(garbage);
-                    getHand(eHand, nHand, wHand, sHand);    
+                    game.setBoard(data);
+                    $("#trash").html(game.getGarbage());
+                    game.getHand();    
                 });
             });
             $(WID).click({hand: i, player: j = 2},function(e){
@@ -94,13 +95,9 @@ $(document).ready(function(){
                 console.log("(Player: " + x + ", Tile: " + y + ") Clicked");
                 $.get("/handle", {"x": x, "y": y, "btn": "left"}, function(response){
                     var data = JSON.parse(response);
-                    var garbage = data["garbage"];
-                    var eHand = data["eastHand"];
-                    var nHand = data["northHand"];
-                    var wHand = data["westHand"];
-                    var sHand = data["southHand"];
-                    $("#trash").html(garbage);
-                    getHand(eHand, nHand, wHand, sHand);    
+                    game.setBoard(data);
+                    $("#trash").html(game.getGarbage());
+                    game.getHand();     
                 });
             });
             $(NID).click({hand: i, player: j = 3},function(e){
@@ -109,13 +106,9 @@ $(document).ready(function(){
                 console.log("(Player: " + x + ", Tile: " + y + ") Clicked");
                 $.get("/handle", {"x": x, "y": y, "btn": "left"}, function(response){
                     var data = JSON.parse(response);
-                    var garbage = data["garbage"];
-                    var eHand = data["eastHand"];
-                    var nHand = data["northHand"];
-                    var wHand = data["westHand"];
-                    var sHand = data["southHand"];
-                    $("#trash").html(garbage);
-                    getHand(eHand, nHand, wHand, sHand);    
+                    game.setBoard(data);
+                    $("#trash").html(game.getGarbage());
+                    game.getHand();    
                 });
             });
         }
