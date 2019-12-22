c54dbe11a42d25b5297fd3d298a78789cda73f16
Solobrosco <nikkosolon24@gmail.com>
2019-12-21 17:46:12 -0800

    included magic numbers for MahjongTiles.h

--- inc/MahjongTiles.h
+++ inc/MahjongTiles.h
@@ -4,6 +4,22 @@
 #include <server.h>
 #include <vector>
 
+/*
+Tiles are numbered accordingly
+Bamboo
+    1-36
+Characters
+    37-72
+Dots
+    72-108
+Honors
+    109-124
+Dragons
+    125-136
+Bonus
+    137-144
+*/
+
 class MahjongTiles{
     MahjongTiles();
     std::vector<int> T;
