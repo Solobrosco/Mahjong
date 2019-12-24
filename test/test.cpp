06d1ad5c004384891b460beeb3b7ca5d1c002b60
Solobrosco <nikkosolon24@gmail.com>
2019-12-23 22:53:48 -0800

    Added 3 unit test for Player class

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,23 @@
 #include <igloo/igloo.h>
+#include <Player.h>
+#include <stdio.h>
 
 using namespace igloo;
+
+Context(PlayerTest){
+	Spec(CanGetNumHand){
+		Player player = Player("East");
+		Assert::That(player.getnumHand(), Equals(14));
+	}
+	Spec(CanGetTurnEast){
+		Player player = Player("East");
+		Assert::That(player.getTurn(), Equals(true));
+	}
+	Spec(CanGetTurnSouth){
+		Player player = Player("South");
+		Assert::That(player.getTurn(), Equals(false));
+	}
+};
 
 int main() {
 	// Run all the tests defined above
