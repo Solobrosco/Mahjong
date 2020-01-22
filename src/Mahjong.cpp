5c71568be9dd0392ff5c86963beab69b4de62933
Solobrosco <nikkosolon24@gmail.com>
2020-01-22 02:43:24 -0800

    Implemented reset() and part of dealHands() which is disabled on Mahjong.cpp

--- src/Mahjong.cpp
+++ src/Mahjong.cpp
@@ -1,33 +1,30 @@
 #include <Mahjong.h>
 
 Mahjong::Mahjong(){
-	// std::cout << "Setting up a game of Mahjong..." << std::endl;
+	std::cout << "Setting up a game of Mahjong..." << std::endl;
+    std::cout << "Getting players..." << std::endl;
     East = new Player("East");
     South = new Player("South");
     West = new Player("West");
     North = new Player("North");
+    playerSet = {East, South, West, North};
+    std::cout << "Setting up tiles..." << std::endl;
     tiles = new MahjongTiles(144);
     state = {"East's Turn","South's Turn","West's Turn","North's Turn","Mahjong!"};
-    shuffleTiles();
-    dealHands();
 }
 
 //================================================================================================================================
 
 void Mahjong::reset(){
-    // std::cout << "Resetting Game..." << std::endl;
-    delete East;
-    delete South;
-    delete West;
-    delete North;
-    delete tiles;
-    tiles = new MahjongTiles(numTiles);
-    state = {"East's Turn","South's Turn","West's Turn","North's Turn","Mahjong!"};
-    East = new Player("East");
-    South = new Player("South");
-    West = new Player("West");
-    North = new Player("North");
-    shuffleTiles();
+    std::cout << "Resetting Game..." << std::endl;
+    tiles->resetTiles();
+
+    East->resetPlayer();
+    South->resetPlayer();
+    West->resetPlayer();
+    North->resetPlayer();
+
+    tiles->shuffleTiles();
     dealHands();
 }
 
@@ -37,9 +34,9 @@ void Mahjong::handle(int x,int y, MouseButton click){
     // y: {(0-13,hand)(14,Chow)(15,Pong)(16,Kong)
     // Ex: (2,5,left) = left click on South hand position 6
     // Ex: (3,14) = West calls Chow
-    if(tiles->getAmount() > 0 && click == left){    
+    if(tiles->getTileSetAmount() > 0 && click == left){    
         std::mt19937 rng (std::random_device{}());
-        std::uniform_int_distribution<> tileSet (0, tiles->getAmount() - 1);
+        std::uniform_int_distribution<> tileSet (0, tiles->getTileSetAmount() - 1);
         int randNum = tileSet(rng);
         int h = tiles->getTile(randNum);
         if(East->getTurn() && x == 0){
@@ -106,49 +103,60 @@ ucm::json Mahjong::getBoard(){
     return result;
 }
 
-void Mahjong::shuffleTiles(){
-    // Random # generator
-    std::mt19937 rng (std::random_device{}());
-    int r,h;
-    // each player should have 13 tiles with starting player with 14 to throw away 1
-    std::uniform_int_distribution<> tileSet (0, tiles->getAmount() - 1);
-    r = tileSet(rng);
-    h = tiles->getTile(r);
-    tiles->rmTile(r);
-    tiles->setGarbage(h);
-}
-
 void Mahjong::dealHands(){
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 14; j++){
-            // std::cout << tiles->getAmount() << std::endl;
-            if(i == 0){
-                std::uniform_int_distribution<> tileSet (0, tiles->getAmount() - 1);
-                r = tileSet(rng);
-                h = tiles->getTile(r);
-                tiles->rmTile(r);
-                East->setHand(h);
-            }else if(i == 1){
-                std::uniform_int_distribution<> tileSet (0, tiles->getAmount() - 1);
-                r = tileSet(rng);
-                h = tiles->getTile(r);
-                tiles->rmTile(r);
-                North->setHand(h);
-            }else if(i == 2){
-                std::uniform_int_distribution<> tileSet (0, tiles->getAmount() - 1);
-                r = tileSet(rng);
-                h = tiles->getTile(r);
-                tiles->rmTile(r);
-                West->setHand(h);
-            }else if(i == 3){
-                std::uniform_int_distribution<> tileSet (0, tiles->getAmount() - 1);
-                r = tileSet(rng);
-                h = tiles->getTile(r);
-                tiles->rmTile(r);
-                South->setHand(h);
-            }
-        }
-    }
+    // do{
+    //     for(int i = 0; i < playerSet->size(); i++){
+    //         while(playerSet[i]->getTurn() == true){ // on turn
+    //             // draw 8 from front of tileset
+    //             if(i < playerSet->size() - 1){
+    //                 playerSet[i]->setTurn();
+    //                 playerSet[i+1]->setTurn();
+    //             }else{
+    //                 playerSet[i]->setTurn();
+    //                 playerSet[0]->setTurn();
+    //             }
+    //         }
+    //     }
+    // }while(East->getNumHand() != 16 && South->getNumHand() != 16 && West->getNumHand() != 16 && North->getNumHand() != 16);
+    // // Random # generator
+    // std::mt19937 rng (std::random_device{}());
+    // int r,h;
+    // // each player should have 13 tiles with starting player with 14 to throw away 1
+    // std::uniform_int_distribution<> tileSet (0, tiles->getTileSetAmount() - 1);
+    // r = tileSet(rng);
+    // h = tiles->getTile(r);
+    // tiles->rmTile(r);
+    // tiles->setGarbage(h);
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 14; j++){
+    //         // std::cout << tiles->getAmount() << std::endl;
+    //         if(i == 0){
+    //             std::uniform_int_distribution<> tileSet (0, tiles->getTileSetAmount() - 1);
+    //             r = tileSet(rng);
+    //             h = tiles->getTile(r);
+    //             tiles->rmTile(r);
+    //             East->setHand(h);
+    //         }else if(i == 1){
+    //             std::uniform_int_distribution<> tileSet (0, tiles->getTileSetAmount() - 1);
+    //             r = tileSet(rng);
+    //             h = tiles->getTile(r);
+    //             tiles->rmTile(r);
+    //             North->setHand(h);
+    //         }else if(i == 2){
+    //             std::uniform_int_distribution<> tileSet (0, tiles->getTileSetAmount() - 1);
+    //             r = tileSet(rng);
+    //             h = tiles->getTile(r);
+    //             tiles->rmTile(r);
+    //             West->setHand(h);
+    //         }else if(i == 3){
+    //             std::uniform_int_distribution<> tileSet (0, tiles->getTileSetAmount() - 1);
+    //             r = tileSet(rng);
+    //             h = tiles->getTile(r);
+    //             tiles->rmTile(r);
+    //             South->setHand(h);
+    //         }
+    //     }
+    // }
 }
 
 bool Mahjong::checkBonuses(int t){
